f1b0ea687c695f68f8094b32833c4b533e812b5b
Omkar <omkar2797.od@gmail.com>
2025-09-18 16:03:38 +0530

    [IMP] Improved.

--- tailor_management/models/sale.py
+++ tailor_management/models/sale.py
@@ -1,12 +1,47 @@
 # -*- coding: utf-8 -*-
 
 from odoo import fields, models, api, _
-
+from odoo.tools import (
+    create_index,
+    float_is_zero,
+    format_amount,
+    format_date,
+    is_html_empty,
+    SQL,
+)
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     child_partner_ids = fields.Many2many('res.partner', 'sale_res_partner_tailor_rel', 'order_id', 'partner_id', 'Related Customers', tracking=True)
+
+    def _get_invoiceable_lines(self, final=False):
+        """Return the invoiceable lines for order `self`."""
+        down_payment_line_ids = []
+        invoiceable_line_ids = []
+        pending_section = None
+        precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
+
+        for line in self.order_line:
+            if line.display_type == 'line_section':
+                # Only invoice the section if one of its lines is invoiceable
+                pending_section = line
+                continue
+            if line.display_type != 'line_note' and float_is_zero(line.qty_to_invoice, precision_digits=precision):
+                continue
+            if line.qty_to_invoice > 0 or (line.qty_to_invoice < 0 and final) or line.display_type == 'line_note':
+                if line.is_downpayment:
+                    # Keep down payment lines separately, to put them together
+                    # at the end of the invoice, in a specific dedicated section.
+                    down_payment_line_ids.append(line.id)
+                    continue
+                if pending_section:
+                    invoiceable_line_ids.append(pending_section.id)
+                    pending_section = None
+                if line.product_id.is_main_product:
+                    invoiceable_line_ids.append(line.id)
+        return self.env['sale.order.line'].browse(invoiceable_line_ids + down_payment_line_ids)
+
 
     def _prepare_invoice(self):
         invoice_vals = super(SaleOrder, self)._prepare_invoice()
