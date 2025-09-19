f18c31e284bb73e0ae4e8172fcc89fe3be9e42d1
Omkar <omkar2797.od@gmail.com>
2025-09-19 20:33:36 +0530

    [ADD] Added.

--- gl_jazzy_theme_extension/__manifest__.py
+++ gl_jazzy_theme_extension/__manifest__.py
@@ -11,9 +11,11 @@
     ],
     'assets': {
         'web.assets_backend': [
+            'gl_jazzy_theme_extension/static/src/scss/variables.scss',
             'gl_jazzy_theme_extension/static/src/scss/sidebar.scss',
             'gl_jazzy_theme_extension/static/src/scss/kanban.scss',
             'gl_jazzy_theme_extension/static/src/scss/list.scss',
+            'gl_jazzy_theme_extension/static/src/scss/dashboard_bg.scss',
             'gl_jazzy_theme_extension/static/src/components/app_menu/side_menu.xml',
         ],
         'web.assets_frontend': [
